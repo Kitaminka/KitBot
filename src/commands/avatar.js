446fb0794a120c0e4b3e0e46386b6757bfd861d9
Kitaminka <whitehacker638@gmail.com>
2020-12-16 10:05:02 +0200

    Fixed avatar command

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -9,22 +9,16 @@ module.exports = {
     description: 'Display the user\'s avatar.',
     async execute(message, args) {
         let selectedUser, embed;
-        if (!args[0]) {
-            selectedUser = message.author;
-        }
-        else if (message.mentions.users.first()) {
-            selectedUser = message.mentions.users.first();
-        }
-        else if (message.guild.members.cache.get(args[0])) {
-            selectedUser = message.guild.members.cache.get(args[0]).user;
-        }
+        if (!args[0]) selectedUser = message.author;
+        else if (message.mentions.users.first()) selectedUser = message.mentions.users.first();
+        else if (message.guild.members.cache.get(args[0])) selectedUser = message.guild.members.cache.get(args[0]).user;
 
         if (!selectedUser) {
             embed = Embed.errorEmbed('User not found.');
         }
         else {
             embed = new Discord.MessageEmbed()
-                .setTitle(`:bust_in_silhouette:${selectedUser} user avatar`)
+                .setTitle(`:bust_in_silhouette:${selectedUser.user.username} user avatar`)
                 .setImage(selectedUser.displayAvatarURL({ format:'png', dynamic:true, size:4096, }))
                 .setTimestamp()
                 .setColor(config.embedColor);
